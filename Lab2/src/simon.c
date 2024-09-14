c9c1019b41de22d0fe733208c516c5557e107c12
Sebastian <sebasvq106@gmail.com>
2024-09-14 13:26:39 -0600

    Lab2
    Signed-off-by: Sebastian <sebasvq106@gmail.com>

--- Lab2/src/simon.c
+++ Lab2/src/simon.c
@@ -1,9 +1,6 @@
 #include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h> 
-
-
-#define BLINK_DELAY 10000 
+#include <avr/interrupt.h>
+#include <stdlib.h>
 
 typedef enum {
     IDLE,
@@ -12,51 +9,113 @@ typedef enum {
 } Estado;
 
 // Variables globales para los contadores
-
 uint8_t secuencia_leds[14];
 uint8_t secuencia_botones[14];
 uint8_t indice_input = 0;
 uint8_t NUM_LEDS = 4;
-uint16_t LED_DELAY = 20000;
+uint16_t LED_DELAY = 2000;
+
 Estado estado_actual = IDLE;
 
 // Array que contiene los pines de los LEDs (PB3, PB4, PB5, PB6)
 uint8_t led_pins[4] = { PB3, PB4, PB5, PB6 };
 
+// Variables para los tres tipos de delays
+volatile uint16_t delay_counter_blink = 0;     // Para el parpadeo
+volatile uint16_t delay_counter_sequence = 0;   // Para la secuencia dinámica
+volatile uint16_t delay_counter_button = 0;     // Para la espera tras presionar el botón
+
+// Funcion que enciende un Led
 void turn_on_leds(uint8_t led_mask) {
     PORTB = led_mask;
 }
 
-void delay_ms_variable(uint16_t delay) {
-    while (delay--) {
-        _delay_ms(1);  // Esperar 1 ms en cada iteración
+// Delay para el parpadeo de LEDs
+void start_blink_delay(uint16_t delay_ms) {
+    delay_counter_blink = delay_ms / 10;
+}
+
+uint8_t is_blink_delay_done() {
+    return delay_counter_blink == 0;
+}
+
+// Delay dinámico para la secuencia de LEDs
+void start_sequence_delay() {
+    delay_counter_sequence = LED_DELAY / 10;
+}
+
+uint8_t is_sequence_delay_done() {
+    return delay_counter_sequence == 0;
+}
+
+// Delay tras presionar un botón
+void start_button_delay(uint16_t delay_ms) {
+    delay_counter_button = delay_ms / 10;
+}
+
+uint8_t is_button_delay_done() {
+    return delay_counter_button == 0;
+}
+
+// Configurar el Timer1 en modo CTC
+void setup_timer1() {
+    TCCR1B |= (1 << WGM12);          		// Configurar Timer1 en modo CTC
+    TCCR1B |= (1 << CS12) | (1 << CS10); 	// Prescaler de 1024
+    OCR1A = 156;                     		// Valor de comparación para generar una interrupción cada ~10ms
+    TIMSK |= (1 << OCIE1A);          		// Habilitar interrupción por comparación
+}
+
+// Configurar interrupciones externas para INT0 e INT1 en flanco ascendente
+void setup_external_interrupts() {
+    MCUCR |= (1 << ISC00) | (1 << ISC10);  // Configurar INT0 e INT1 para detectar flanco ascendente
+    GIMSK |= (1 << INT0) | (1 << INT1);    // Habilitar interrupciones INT0 e INT1
+}
+
+
+// ISR para manejar los contadores de los delays
+ISR(TIMER1_COMPA_vect) {
+    if (delay_counter_blink > 0) {
+        delay_counter_blink--;
+    }
+
+    if (delay_counter_sequence > 0) {
+        delay_counter_sequence--;
+    }
+
+    if (delay_counter_button > 0) {
+        delay_counter_button--;
     }
 }
 
+// Mostrar la secuencia aleatoria de LEDs con delays dinámicos
 void show_random_sequence() {
+    if (NUM_LEDS == 4) {
+        for (uint8_t i = 0; i < NUM_LEDS; i++) {
+            uint8_t random_index = rand() % 4;
+            secuencia_leds[i] = random_index;
+            turn_on_leds(1 << led_pins[random_index]);
 
-    if (NUM_LEDS == 4){
-	    for (uint8_t i = 0; i < NUM_LEDS; i++) {
-		uint8_t random_index = rand() % 4;
-		uint8_t led = (1 << led_pins[random_index]);
-		secuencia_leds[i] = random_index;
-		turn_on_leds(led);
-		delay_ms_variable(LED_DELAY);
-		PORTB = 0x00; // Apagar todos los LEDs
-		_delay_ms(10000); // Tiempo de espera entre LEDs
-	    }
+            start_sequence_delay(); 
+            while (!is_sequence_delay_done());
+
+            PORTB = 0x00; // Apagar LEDs
+            start_blink_delay(1000); // Tiempo entre LEDs
+            while (!is_blink_delay_done()); // Esperar el tiempo de apagado
+        }
     } else {
-    
-    uint8_t random_index = rand() % 4;
-    secuencia_leds[NUM_LEDS - 1] = random_index;
-    for (uint8_t i = 0; i < NUM_LEDS; i++) {
-        uint8_t led = (1 << led_pins[secuencia_leds[i]]);
-        turn_on_leds(led);
-        delay_ms_variable(LED_DELAY);
-        PORTB = 0x00;  // Apagar todos los LEDs
-        _delay_ms(10000);  // Tiempo de espera entre LEDs
+        secuencia_leds[NUM_LEDS - 1] = rand() % 4;
+
+        for (uint8_t i = 0; i < NUM_LEDS; i++) {
+            turn_on_leds(1 << led_pins[secuencia_leds[i]]);
+
+            start_sequence_delay(); // Iniciar delay dinámico
+            while (!is_sequence_delay_done()); // Esperar a que termine el delay
+
+            PORTB = 0x00;  // Apagar todos los LEDs
+            start_blink_delay(1000); // Tiempo de espera entre LEDs
+            while (!is_blink_delay_done()); // Esperar el tiempo de apagado
+        }
     }
-   }
 }
 
 uint8_t comparar_secuencias() {
@@ -74,10 +133,12 @@ int main(void) {
 
     // Configurar los pines PD2, PD3, PD4 y PD5 como entradas para los botones
     DDRD &= ~((1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5));
+    
     // Habilitar resistencias pull-up internas
     PORTD |= (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
-	
-    // Estado estado_actual = IDLE;
+
+    setup_timer1(); // Configurar el Timer1
+    sei(); // Habilitar interrupciones globales
 
     while (1) {
         switch (estado_actual) {
@@ -87,9 +148,12 @@ int main(void) {
                     // Parpadear todos los LEDs 2 veces
                     for (uint8_t i = 0; i < 2; i++) {
                         turn_on_leds((1 << PB3) | (1 << PB4) | (1 << PB5) | (1 << PB6)); // Encender todos los LEDs
-                        _delay_ms(10000); // Esperar 10000 ms (10 segundos)
+                        start_blink_delay(1000); // Iniciar delay de 1 segundo
+                        while (!is_blink_delay_done()); // Esperar a que el delay termine
+
                         PORTB = 0x00; // Apagar todos los LEDs
-                        _delay_ms(10000); // Esperar 10000 ms (10 segundos)
+                        start_blink_delay(1000); // Iniciar delay de 1 segundo
+                        while (!is_blink_delay_done()); // Esperar a que el delay termine
                     }
                     estado_actual = SECUENCIA;
                 }
@@ -97,55 +161,62 @@ int main(void) {
 
             case SECUENCIA:
                 show_random_sequence();
-                estado_actual = INPUT; 
+                estado_actual = INPUT;
                 break;
-                
-           case INPUT: 
-           	while(indice_input < NUM_LEDS) {
-           		if ((PIND & (1 << PD5))) {
-           		secuencia_botones[indice_input] = 3;
-           		_delay_ms(5000);
-           		indice_input++;
-           		}
-           		
-           		if ((PIND & (1 << PD4))){
-           		secuencia_botones[indice_input] = 2;
-           		_delay_ms(5000);
-           		indice_input++;
-           		}
-           		
-           		if ((PIND & (1 << PD3))){
-           		secuencia_botones[indice_input] = 1;
-           		_delay_ms(5000);
-           		indice_input++;
-           		}
-           		
-           		if ((PIND & (1 << PD2))){
-           		secuencia_botones[indice_input] = 0;
-           		_delay_ms(5000);
-           		indice_input++;
-           		}
-           		estado_actual = INPUT;
-           	}
-           	
-           	if (comparar_secuencias()){
-           		indice_input = 0;
-           		NUM_LEDS++;
-           		LED_DELAY = LED_DELAY - 2000;
-           		estado_actual = SECUENCIA;
-           	} else {
-           	
-           		for (uint8_t i = 0; i < 3; i++) {
+
+            case INPUT:
+                while (indice_input < NUM_LEDS) {
+                    if ((PIND & (1 << PD5))) {
+                        secuencia_botones[indice_input] = 3;
+                        start_button_delay(500); // Iniciar delay de 500 ms tras pulsar el botón
+                        while (!is_button_delay_done()); // Esperar
+                        indice_input++;
+                    }
+
+                    if ((PIND & (1 << PD4))) {
+                        secuencia_botones[indice_input] = 2;
+                        start_button_delay(500); // Iniciar delay de 500 ms tras pulsar el botón
+                        while (!is_button_delay_done()); // Esperar
+                        indice_input++;
+                    }
+
+                    if ((PIND & (1 << PD3))) {
+                        secuencia_botones[indice_input] = 1;
+                        start_button_delay(500); // Iniciar delay de 500 ms tras pulsar el botón
+                        while (!is_button_delay_done()); // Esperar
+                        indice_input++;
+                    }
+
+                    if ((PIND & (1 << PD2))) {
+                        secuencia_botones[indice_input] = 0;
+                        start_button_delay(500); // Iniciar delay de 500 ms tras pulsar el botón
+                        while (!is_button_delay_done()); // Esperar
+                        indice_input++;
+                    }
+                }
+
+                // Comparar secuencias
+                if (comparar_secuencias()) {
+                    indice_input = 0;
+                    NUM_LEDS++;
+                    LED_DELAY -= 200; // Disminuir el delay dinámico
+                    estado_actual = SECUENCIA;
+                } else {
+                    for (uint8_t i = 0; i < 3; i++) {
                         turn_on_leds((1 << PB3) | (1 << PB4) | (1 << PB5) | (1 << PB6)); // Encender todos los LEDs
-                        _delay_ms(10000); // Esperar 10000 ms (10 segundos)
+                        start_blink_delay(1000); // Iniciar delay de 1 segundo
+                        while (!is_blink_delay_done()); // Esperar a que el delay termine
+
                         PORTB = 0x00; // Apagar todos los LEDs
-                        _delay_ms(10000); // Esperar 10000 ms (10 segundos)
-                    	}
-                    	indice_input = 0;
-                    	NUM_LEDS = 4;
-                    	LED_DELAY = 20000;
-		   	estado_actual = IDLE;
-           	}
+                        start_blink_delay(1000); // Iniciar delay de 1 segundo
+                        while (!is_blink_delay_done()); // Esperar a que el delay termine
+                    }
+
+                    indice_input = 0;
+                    NUM_LEDS = 4;
+                    LED_DELAY = 20000;
+                    estado_actual = IDLE;
+                }
                 break;
         }
     }
