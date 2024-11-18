01aacacdad8a7f65a2cd08d4298d64a8dbe158e1
Sebastian <sebasvq106@gmail.com>
2024-11-18 17:00:35 -0600

    Version 2
    Signed-off-by: Sebastian <sebasvq106@gmail.com>

---
+++ Lab5/main_functions.cc
@@ -0,0 +1,110 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "main_functions.h"
+
+#include "tflite_learn_9.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+#include "printf.h"
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+int numSamples = 9;
+int NUM_GESTURES = 3;
+
+// Create an area of memory to use for input, output, and intermediate arrays.
+// Finding the minimum value for your model may require some trial and error.
+constexpr int kTensorArenaSize = 8 * 1024;
+uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
+
+// Movimientos
+
+const char* GESTURES[] = {
+	"arriba-abajo",
+	"derecha-izquierda",
+	"circular"
+};
+
+// The name of this function is important for Arduino compatibility.
+void tensor_setup() {
+  // Set up logging. Google style is to avoid globals or statics because of
+  // lifetime uncertainty, but since this has a trivial destructor it's okay.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(tflite_learn_9);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    error_reporter->Report(
+        "Model provided is schema version %d not equal "
+        "to supported version %d.",
+        model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  // This pulls in all the operation implementations we need.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::AllOpsResolver resolver;
+
+  // Build an interpreter to run the model with.
+  static tflite::MicroInterpreter static_interpreter(
+      model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    error_reporter->Report("AllocateTensors() failed");
+    return;
+  }
+
+  // Obtain pointers to the model's input and output tensors.
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+
+}
+
+void detectar(int samplesRead, int gx, int gy, int gz){
+
+    input->data.f[samplesRead * 3 + 0] = gx;
+    input->data.f[samplesRead * 3 + 1] = gy;
+    input->data.f[samplesRead * 3 + 2] = gz;
+
+    if (samplesRead == numSamples) {
+        TfLiteStatus invoke_status = interpreter->Invoke();
+        if (invoke_status != kTfLiteOk) {
+            error_reporter->Report("Invoke failed");
+            return;
+        }
+
+        for (int i = 0; i < NUM_GESTURES; i++){
+            printf(GESTURES[i]);
+            printf(": ");
+            printf("%f\n\r", output->data.f[i]); 
+        }
+    }
+}
